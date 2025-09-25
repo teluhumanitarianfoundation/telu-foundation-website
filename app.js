b8c4818cc6faa06a08d523a831f94bf8e8fe5b6b
teluhumanitarianfoundation <teluhumanitarianfoundation@gmail.com>
2025-09-25 15:53:50 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -18,3 +18,33 @@ let disappear =document.querySelector('#nav-menu');
         document.querySelector('#hamburger').style.display='block';
         this.style.display='none';
     })
+
+
+
+
+    const buttons = document.querySelectorAll('.amount-btn');
+    const input = document.getElementById('amount-input');
+
+    //  buttons.forEach(button => {
+    //         button.addEventListener('click', () => {
+    //             buttons.forEach(btn => btn.classList.remove('selected'));
+    //             button.classList.add('selected');
+                
+    //             const amount = button.getAttribute('data-amount');
+    //             input.value = `${amount}`;
+    //         });
+    //     });
+
+     buttons.forEach(button => {
+            button.addEventListener('click', () => {
+                if (button.classList.contains('selected')) {
+                    button.classList.remove('selected');
+                    input.value = '';
+                } else {
+                    buttons.forEach(btn => btn.classList.remove('selected'));
+                    button.classList.add('selected');
+                    const amount = button.getAttribute('data-amount');
+                    input.value = amount;
+                }
+            });
+        });
